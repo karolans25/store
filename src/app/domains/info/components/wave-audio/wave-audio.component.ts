efaef1eb7b18008ede7f23e9179990bbad7c5fbe
karolans25 <dev.carolina.pulido01@gmail.com>
2024-08-30 13:48:18 -0500

    class 40 platzi course

---
+++ src/app/domains/info/components/wave-audio/wave-audio.component.ts
@@ -0,0 +1,24 @@
+import { CommonModule } from '@angular/common';
+import { Component, ElementRef, Input, ViewChild } from '@angular/core';
+
+import WaveSurfer from 'wavesurfer.js';
+
+@Component({
+  selector: 'app-wave-audio',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './wave-audio.component.html',
+  styleUrl: './wave-audio.component.css'
+})
+export class WaveAudioComponent {
+  
+  @Input({required: true}) audioUrl!: string;
+  @ViewChild('wave') container!: ElementRef;
+
+  ngAfterViewInit(){
+    WaveSurfer.create({
+      url: this.audioUrl,
+      container: this.container.nativeElement
+    });
+  }
+}

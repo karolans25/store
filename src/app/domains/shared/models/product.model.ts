8efda2ec7d25dd909dac844204378bc61623aae2
karolans25 <dev.carolina.pulido01@gmail.com>
2024-08-30 20:09:11 -0500

    Use parameters as Input, and show info of a product in detail component

--- src/app/domains/shared/models/product.model.ts
+++ src/app/domains/shared/models/product.model.ts
@@ -1,13 +1,11 @@
+import { Category } from "./category.model";
+
 export interface Product {
     id: number;
     title: string;
     price: number;
     description: string;
-    category: {
-        id: number;
-        name: string;
-        image: string;
-    }
+    category: Category;
     images: string[];
     creationAt: string;
 }
